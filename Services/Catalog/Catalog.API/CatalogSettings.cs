c7e508c76fff0f6dcd6471ba6f8446fdad9759ad
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2025-10-07 01:30:46 -0300

    Add Catalog.API CatalogSettings

---
+++ Services/Catalog/Catalog.API/CatalogSettings.cs
@@ -0,0 +1,9 @@
+
+namespace eShop.Services.Catalog.API {
+    public class CatalogSettings {
+        public string PictureBaseURL { get; set; }
+        public string EventBusConnection { get; set; }
+        public bool UseCustomizationData { get; set; }
+        public bool AzureStorageEnabled { get; set; }
+    }
+}
--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -44,6 +44,7 @@ namespace eShop.Services.Catalog.API {
                     }
                 );
             });
+            services.Configure<CatalogSettings>(configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
