13eedcd0ed82fc42566c5d51e327b8870111986e
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2025-11-12 23:18:13 -0300

    Apply abstraction to the IoC container configuration in Catalog.API

---
+++ Services/Catalog/Catalog.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Data.Common;
+using System.IO;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using eShop.BuildingBlocks.EventBus;
+using eShop.BuildingBlocks.EventBus.Abstractions;
+using eShop.BuildingBlocks.EventBusRabbitMQ;
+using eShop.BuildingBlocks.IntegrationEventLogEF;
+using eShop.BuildingBlocks.IntegrationEventLogEF.Services;
+using eShop.Services.Catalog.API.Infrastructure;
+using eShop.Services.Catalog.API.IntegrationEvents;
+using eShop.Services.Catalog.API.IntegrationEvents.EventHandling;
+using eShop.Services.Catalog.API.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
+
+namespace eShop.Services.Catalog.API.Extensions {
+    internal static class ServiceCollectionExtensions {
+        internal static IServiceCollection AddMVC(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddControllers(options => {
+                options.SuppressAsyncSuffixInActionNames = true;
+            });
+
+            return services;
+        }
+
+        internal static IServiceCollection AddRPC(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddGrpc();
+
+            return services;
+        }
+
+        internal static IServiceCollection AddDBContext(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddDbContext<CatalogContext>(options => {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("SQLServer"),
+                    action => {
+                        action.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name);
+                        action.EnableRetryOnFailure(
+                            maxRetryCount: 15,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null
+                        );
+                    }
+                );
+            });
+            services.AddDbContext<IntegrationEventLogContext>(options => {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("SQLServer"),
+                    action => {
+                        action.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name);
+                        action.EnableRetryOnFailure(
+                            maxRetryCount: 15,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null
+                        );
+                    }
+                );
+            });
+
+            return services;
+        }
+
+        internal static IServiceCollection AddSwagger(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo {
+                    Version = "v1",
+                    Title = "Catalog Service",
+                    Description = "An ASP.NET Core Web API for managing a catalog of products"
+                });
+
+                string xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+            });
+
+            return services;
+        }
+
+        internal static IServiceCollection AddOptions(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.Configure<CatalogSettings>(configuration);
+
+            return services;
+        }
+
+        internal static IServiceCollection AddIntegrationServices(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddTransient<Func<DbConnection, IIntegrationEventLogService>>(
+                x => (DbConnection connection) => new IntegrationEventLogService(connection)
+            );
+            services.AddTransient<ICatalogIntegrationEventService, CatalogIntegrationEventService>();
+
+            return services;
+        }
+
+        internal static IServiceCollection AddEventBus(this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddSingleton<IRabbitMQPersistentConnection>(x => {
+                CatalogSettings settings = x.GetRequiredService<IOptions<CatalogSettings>>().Value;
+                ILogger<DefaultRabbitMQPersistentConnection> logger =
+                    x.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+                ConnectionFactory factory = new ConnectionFactory() {
+                    HostName = configuration["EventBusConnection"]
+                };
+
+                if (!string.IsNullOrEmpty(configuration["EventBusUserName"])) {
+                    factory.UserName = configuration["EventBusUserName"];
+                }
+
+                if (!string.IsNullOrEmpty(configuration["EventBusPassword"])) {
+                    factory.UserName = configuration["EventBusPassword"];
+                }
+
+                int retryCount = 5;
+                if (!string.IsNullOrEmpty(configuration["EventBusRetryCount"])) {
+                    retryCount = int.Parse(configuration["EventBusRetryCount"]);
+                }
+
+                return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
+            });
+            services.AddSingleton<IEventBus, EventBusRabbitMQ>(serviceProvider => {
+                IRabbitMQPersistentConnection connection = serviceProvider.GetRequiredService<IRabbitMQPersistentConnection>();
+                ILifetimeScope lifetimeScope = serviceProvider.GetRequiredService<ILifetimeScope>();
+                ILogger<EventBusRabbitMQ> logger = serviceProvider.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+                IEventBusSubscriptionManager subscriptionManager = serviceProvider.GetRequiredService<IEventBusSubscriptionManager>();
+
+                int retryCount = 5;
+                if (!string.IsNullOrEmpty(configuration["EventBusRetryCount"])) {
+                    retryCount = int.Parse(configuration["EventBusRetryCount"]);
+                }
+
+                return new EventBusRabbitMQ(
+                    connection, logger, lifetimeScope, subscriptionManager,
+                    configuration["SubscriptionClientName"], retryCount
+                );
+            });
+
+            services.AddSingleton<IEventBusSubscriptionManager, InMemoryEventBusSubscriptionManager>();
+            services.AddTransient<ProductPriceChangedIntegrationEventHandler>();
+
+            return services;
+        }
+
+        internal static IServiceProvider AddAutofacModules(this IServiceCollection services,
+            IConfiguration configuration) {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.Populate(services);
+
+            return new AutofacServiceProvider(containerBuilder.Build());
+        }
+    }
+}
