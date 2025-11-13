09230fb3a9f9c19b214c209cf6c8fd83ffed470f
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2025-11-13 00:19:33 -0300

    Add Ordering.Domain exceptions

---
+++ Services/Ordering/Ordering.Domain/Exceptions/OrderingDomainException.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace eShop.Services.Ordering.Domain.Exceptions {
+    /// <summary>
+    /// Exception type for domain exceptions
+    /// </summary>
+    public class OrderingDomainException : Exception {
+        public OrderingDomainException() { }
+        public OrderingDomainException(string message) : base(message) { }
+        public OrderingDomainException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
