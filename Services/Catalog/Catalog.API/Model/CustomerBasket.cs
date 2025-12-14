0ff5382916cf8feffe67ad7b94f2618567919081
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2025-12-14 12:29:37 -0300

    Add missing Catalog.API models

---
+++ Services/Catalog/Catalog.API/Model/CustomerBasket.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace eShop.Services.Catalog.API.Model {
+    public class CustomerBasket {
+        public CustomerBasket() {
+            this.BasketItems = new List<BasketItem>();
+        }
+
+        public CustomerBasket(string customerID) {
+            this.BuyerID = customerID;
+        }
+
+        public string BuyerID { get; set; }
+        public List<BasketItem> BasketItems { get; set; }
+    }
+}
